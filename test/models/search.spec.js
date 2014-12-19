5fb5a88e1cb7f08e01bf0eff49f32d6ae7d8b724
fendyg <fendygiovanni@gmail.com>
2014-12-19 09:43:39 -0800

    test added for chapter 3

--- test/models/search.spec.js
+++ test/models/search.spec.js
@@ -1,10 +1,22 @@
 /*global beforeEach, describe, it, assert, expect  */
 'use strict';
 
-describe('Search Model', function () {
+define([
+    'models/search'
+    ],function(SearchModel){
 
-    beforeEach(function () {
-        this.SearchModel = new SpotsearchTutorial.Models.Search();
-    });
+        describe('Search Model', function(){
 
+            beforeEach(function(){
+                this.searchModel = new SearchModel;
+            });
+
+            it('should update the url when query is changed',function(){
+                this.searchModel.query = 'jack';
+                this.searchModel.types = 'album';
+                var url = this.searchModel.url();
+
+                expect(url).to.equal('https://api.spotify.com/v1/search?q=jack&type=album');
+            });
+        });
 });
